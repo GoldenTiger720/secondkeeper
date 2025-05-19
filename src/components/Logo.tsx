666524d85e4fdf6c5e95a27f51c068c744747bca
Sergei928 <sergeiromanoff.job@gmail.com>
2025-05-19 11:08:43 +0300

    Deleted Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,6 @@ export function Logo({ className }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <Link to="/" className={`flex items-center space-x-2 ${className}`}>
       <div className="flex items-center">
-        <img src="icon.png" alt="SecondKeeper Logo Icon" className="h-8 w-8" />
         <img
           src="/Logo.png"
           alt="SecondKeeper Logo Text"
